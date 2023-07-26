544375f23097a967ea0246890cb6862c3a455a49
dap-protos <tom@subfusion.com>
2023-07-26 16:11:05 +0100

    Keying sheet generator

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -431,8 +431,8 @@ module.exports = {
     'facility-18-exposure': '576,000.00',
     'facility-18-paid': '1,500.00',
     'facility-18-accrued': '1,500.00',
-    'facility-18-paid-total': '1,400.00',
-    'facility-18-paid-subtotal': '1,500.00',
+    'facility-18-paid-total': '1,500.00',
+    'facility-18-paid-subtotal': '1,400.00',
 
     // FACILITY 19 VALUES
     'facility-19-updated': 'no',
