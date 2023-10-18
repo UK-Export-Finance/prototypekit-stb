b53922e0a4478fe582f4dfdf79bf1de062594e82
Tom <tom@subfusion.com>
2023-10-18 16:21:34 +0100

    Updates including filter and truncated dates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,20 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   window.MOJFrontend.initAll();
 });
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: "(min-width: 48.063em)",
+  startHidden: false,
+  toggleButton: {
+    container: $(".moj-action-bar__filter"),
+    showText: "Show filter",
+    hideText: "Hide filter",
+    classes: "govuk-button--secondary",
+  },
+  closeButton: {
+    container: $(".moj-filter__header-action"),
+    text: "Close",
+  },
+  filter: {
+    container: $(".moj-filter"),
+  },
+});
